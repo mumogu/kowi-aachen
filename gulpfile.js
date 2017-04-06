7567787aa11212b0f6365521ec3dfca03f08e974
Dirk <dirk@popskorn.de>
2017-04-06 19:53:16 +0200

    Working on kowi splash page

--- gulpfile.js
+++ gulpfile.js
@@ -11,38 +11,38 @@ gulp.task('build', ['jekyll', 'sass', 'img', 'babel'], () => {
 })
 
 gulp.task('serve', () => {
-    browserSync.init({
-        files: 'dist/**',
-        port: 4000,
-        server: {
-            baseDir: 'dist'
-        }
-    })
+	browserSync.init({
+		files: 'dist/**',
+		port: 4000,
+		server: {
+			baseDir: 'dist'
+		}
+	})
 
-    gulp.watch('src/**/*', ['jekyll', 'sass', 'img', 'babel'])
+	gulp.watch('src/**/*', ['jekyll', 'sass', 'img', 'babel'])
 })
 
 gulp.task('jekyll', () => {
-    child.execSync('cd src && jekyll build', (err, stdout, stderr) => {
-        gutil.log(stdout)
-        gutil.log(stderr)
-    })
+	child.execSync('cd src && jekyll build', (err, stdout, stderr) => {
+    gutil.log(stdout)
+    gutil.log(stderr)
+})
 })
 
 gulp.task('sass', () => {
-    return gulp.src('src/assets/scss/*.scss')
-        .pipe(sass())
-        .pipe(concat('style.css'))
-        .pipe(gulp.dest('dist/css'))
+	return gulp.src('src/assets/scss/*.scss')
+		.pipe(sass())
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('dist/css'))
 })
 
 gulp.task('img', () => {
-    return gulp.src('src/assets/img/*.*')
-        .pipe(gulp.dest('dist/img'))
+	return gulp.src('src/assets/img/*.*')
+		.pipe(gulp.dest('dist/img'))
 })
 
 gulp.task('babel', () => {
-    return gulp.src('src/assets/js/**/*.js')
+	 return gulp.src('src/assets/js/**/*.js')
         .pipe(babel())
         .pipe(gulp.dest('dist/js'))
 })
