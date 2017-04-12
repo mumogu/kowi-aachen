f808e8c5bb3aa84f2a9a2a5625790c88c07f04a4
Dirk <dirk@popskorn.de>
2017-04-12 15:16:19 +0200

    Cognito auth on every page.

--- src/assets/js/cognito.js
+++ src/assets/js/cognito.js
@@ -1,5 +1,61 @@
 $(document).ready(function () {
 
+    // Check if user is logged in from local storage
+    var poolData = {
+        UserPoolId: 'eu-central-1_DX2w1mj2o', // Your user pool id here
+        ClientId: '5dfoc069ntodvdsdvm3jt34sde' // Your client id here
+    };
+    var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+    var cognitoUser = userPool.getCurrentUser();
+
+    if (cognitoUser != null) {
+        cognitoUser.getSession(function (err, session) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('session validity: ' + session.isValid());
+
+            if(session.isValid()) {
+                $('.nav-login').html('<a href="#">Logout</a>')
+            }
+
+            // NOTE: getSession must be called to authenticate user before calling getUserAttributes
+            cognitoUser.getUserAttributes(function (err, attributes) {
+                if (err) {
+                    console.log('Error getting user attributes: ' + err)
+                } else {
+                    console.log('User Attributes: ' + attributes)
+                }
+            });
+
+            /*
+            AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                IdentityPoolId: '...', // your identity pool id here
+                Logins: {
+                    // Change the key below according to the specific region your user pool is in.
+                    'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>': session.getIdToken().getJwtToken()
+                }
+            });
+            */
+
+            // Instantiate aws sdk service objects now that the credentials have been updated.
+            // example: var s3 = new AWS.S3();
+
+        });
+    }
+
+    $('.nav-login').click(function() {
+        if($(this).text() == "Logout") {
+            cognitoUser.signOut();
+
+
+            $('.nav-login').html('<a href="/login.html">Login</a>')
+
+        }
+    })
+
+
     $('#login-button').click(function () {
 
         const user_name = $('#login-name').val()
@@ -27,14 +83,18 @@ $(document).ready(function () {
                 console.log('Received Access token.');
 
 
-                AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-                    IdentityPoolId: 'eu-central-1_DX2w1mj2o', // your identity pool id here
-                    Logins: {
-                        // Change the key below according to the specific region your user pool is in.
-                        'cognito-idp.eu-central-1.amazonaws.com/eu-central-1_DX2w1mj2o': result.getIdToken().getJwtToken()
-                    }
-                });
+                $('.nav-login').html('<a href="#">Logout</a>')
 
+
+                /*
+                 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                 IdentityPoolId: 'eu-central-1_DX2w1mj2o', // your identity pool id here
+                 Logins: {
+                 // Change the key below according to the specific region your user pool is in.
+                 'cognito-idp.eu-central-1.amazonaws.com/eu-central-1_DX2w1mj2o': result.getIdToken().getJwtToken()
+                 }
+                 });
+                 */
                 // Instantiate aws sdk service objects now that the credentials have been updated.
                 // example: var s3 = new AWS.S3();
 
